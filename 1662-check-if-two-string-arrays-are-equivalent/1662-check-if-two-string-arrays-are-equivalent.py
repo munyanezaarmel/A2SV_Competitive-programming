f9a27702b3ba101c6cb2d161b70a4c4581d97245
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-02-14 12:11:35 +0200

    Time: 17 ms (51.27%), Space: 11.6 MB (99.93%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+         one="".join(word1)
+         two="".join(word2)
+         if one == two:
+                return True
+         else:
+             return False

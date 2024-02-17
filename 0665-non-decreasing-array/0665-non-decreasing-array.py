fdaa7567a96b0b76735aab3ed1fa7596116b9838
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-02-17 18:58:53 +0200

    Time: 133 ms (72.44%), Space: 13 MB (66.14%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        modified = False  
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                if modified: 
+                    return False
+                if i == 0 or nums[i+1] >= nums[i-1]: 
+                    nums[i] = nums[i+1]
+                else: 
+                    nums[i+1] = nums[i]
+                modified = True
+        return True
+        
+        

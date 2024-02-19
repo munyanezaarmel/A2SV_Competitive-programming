adfb7c415ee4079e9911a4b904df720a88026112
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-02-19 19:51:51 +0200

    contests

--- codeforces contests/B_File_System.py
+++ codeforces contests/B_File_System.py
@@ -1,14 +1,12 @@
 n = int(input())
 
-# Dictionary to store count and index of each file name
 file_counts = {}
 
 for _ in range(n):
     file_name = input()
     
-    # If file_name is not in the dictionary, add it with count 1
     if file_name not in file_counts:
-        file_counts[file_name] = 1
+        file_counts[file_name] = 0
         print("OK")
     else:
         file_counts[file_name] += 1

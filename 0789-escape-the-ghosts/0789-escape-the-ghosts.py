fbfd73a645c2e40340ec89ab9710ef3f8d53520e
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-02-19 14:55:53 +0200

    Time: 29 ms (48.48%), Space: 11.7 MB (42.42%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def escapeGhosts(self, ghosts, target):
+        player_distance = abs(target[0]) + abs(target[1]) 
+        for ghost in ghosts:
+             ghost_distance = abs(ghost[0] - target[0]) + abs(ghost[1] - target[1]) 
+             if ghost_distance <= player_distance:
+                    return False
+
+        return True

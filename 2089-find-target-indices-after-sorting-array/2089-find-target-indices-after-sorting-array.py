8c0b3e4cafb5fc94db9740d59b10ebb29679b6c3
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-02-29 15:54:54 +0200

    Time: 79 ms (5.09%), Space: 11.8 MB (39.53%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -5,10 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        nums_sorted=sorted(nums)
-
+        for i in range(len(nums)):
+            for j in range(len(nums)-i-1):
+                 if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
         result = []
-        for index, value in enumerate(nums_sorted):
+        for index, value in enumerate(nums):
             if value == target:
                 result.append(index)
 

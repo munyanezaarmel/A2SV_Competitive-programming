22847837f8cb20a495e174ae17b97373d64b4ae9
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-03-16 22:13:14 +0200

    Time: 26 ms (26.43%), Space: 11.7 MB (31.92%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def partitionLabels(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        d = collections.defaultdict(int)
+        for i, c in enumerate(s): d[c] = i
+        ans, left, right = [], -1, -1
+        for i, c in enumerate(s):
+            right = max(right, d[c])
+            if i == right:
+                ans.append(right-left)
+                left = i
+        return ans

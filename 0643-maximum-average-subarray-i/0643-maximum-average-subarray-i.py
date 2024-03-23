f98a9d4b0c9b80a35b83336d967e0728dabc4921
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-03-23 11:46:26 +0200

    Time: 914 ms (66.68%), Space: 21.1 MB (50.84%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -5,12 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: float
         """
-        su = 0
-        ma = float('-inf')
-        for i, x in enumerate(nums):
-            su += x
-            if i >= k:
-                su -= nums[i-k]
-            if i >= k - 1:
-                ma = max(ma, su)
-        return ma / float(k)
+        sum_elements=0
+        for i in range(k):
+            sum_elements+=nums[i]
+         
+        maxSum=sum_elements
+        
+        startIndex=0
+        endIndex=k
+        
+        while endIndex<len(nums):
+            sum_elements-=nums[startIndex]
+            startIndex+=1
+            
+            sum_elements+=nums[endIndex]
+            endIndex+=1         
+            maxSum=max(maxSum,sum_elements)
+        
+        return float(maxSum)/k

a90c8131f1f28a9189b32e34914f1fe3c0083f5a
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-05-01 20:53:36 +0200

    Time: 12 ms (73.07%), Space: 11.9 MB (17.97%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,4 +5,11 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        return pow(x,n)
+        if n == 0: return 1
+        if n == 1: return x
+        if n == -1: return 1 / x
+        if n % 2 == 0:
+            return self.myPow(x, n // 2) ** 2
+        return (self.myPow(x, n // 2) ** 2) * x
+    
+    

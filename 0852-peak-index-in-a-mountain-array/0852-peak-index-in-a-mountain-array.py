45c006d3026222748bf22a1ec38695b67919f18c
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-05-29 11:27:06 +0200

    Time: 453 ms (89.61%), Space: 24 MB (76.36%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
-        return arr.index(max(arr))
+        left, right = 0, len(arr) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left

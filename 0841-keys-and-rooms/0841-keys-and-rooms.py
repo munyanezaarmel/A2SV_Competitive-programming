6909a5e59052e417f6d19c7342a02a9411b5c00d
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-06-28 18:26:01 +0200

    Time: 48 ms (22.42%), Space: 12.3 MB (49.19%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        n = len(rooms)
+        visited = set([0])
+        queue = deque([0])
+        
+        while queue:
+            room = queue.popleft()
+            for key in rooms[room]:
+                if key not in visited:
+                    visited.add(key)
+                    queue.append(key)
+        
+        return len(visited) == n
